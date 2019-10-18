b7885ded127ce96debc567a4eef1469fa0d4f9ff
Larisa1992 <pelar1992@mail.ru>
2019-10-18 22:19:18 +0300

    ready for test

--- conftest.py
+++ conftest.py
@@ -1,23 +1,22 @@
 import pytest
+import time
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
-                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store',
+                     help="Choose browser language")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    browser_language = request.config.getoption("language")
+    
+    options = Options()
+    options.add_argument("--lang={}".format(browser_language))
+    options.add_experimental_option('prefs', {'intl.accept_languages': browser_language})
+    print("\nstart chrome browser for language {}".format(browser_language))
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,1 +1,11 @@
+import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_parametrize_language_browser(browser):
+    browser.get(link)
+    time.sleep(30)
+    button_basket = browser.find_elements_by_css_selector(".btn-add-to-basket")
+
+    assert len(button_basket) == 1, "кнопка не одна"
+    
+    
